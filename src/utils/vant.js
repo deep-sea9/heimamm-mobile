0a38c9e4f19af5bc25906d595f4bc15806576d4b
knightallone <wuzhicheng2580@gmail.com>
2022-07-25 21:30:05 +0800

    '我的页面样式完成'

--- src/utils/vant.js
+++ src/utils/vant.js
@@ -2,10 +2,12 @@
  * vant 组件注册
  */
 import Vue from 'vue'
-import { Button, Tabbar, TabbarItem, Icon } from 'vant'
+import { Button, Tabbar, TabbarItem, Icon, Cell, CellGroup } from 'vant'
 
 Vue.use(Button)
 
 Vue.use(Tabbar)
 Vue.use(TabbarItem)
 Vue.use(Icon)
+Vue.use(Cell)
+Vue.use(CellGroup)
