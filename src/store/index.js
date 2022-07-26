4afdf5afbdbb4352312846c31796ac857ab2105d
咻咻啊 <2379410527@qq.com>
2022-07-26 20:18:30 +0800

    数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import persistedstate from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+  plugins: [persistedstate({ paths: ['token'] })]
 })
