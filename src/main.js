081fdd371e6ead861d4242f509fe95d9a88244ee
冯肖骁 <1754152409@qq.com>
2022-07-30 16:19:43 +0800

    卸载插件

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import 'normalize.css'
 import '@/utils/vant'
 import '@/style/index.less'
 import filter from '@/filters'
+import echarts from 'echarts' // 引入echarts
+Vue.prototype.$echarts = echarts // 将echarts挂载到Vue原型上
 Vue.use(filter)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App)
+  render: h => h(App)
 }).$mount('#app')
